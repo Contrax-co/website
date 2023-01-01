fb77f7b7522b1ea7b5b1afdd8c8189bb3ddd09f4
Soheeb Amin <53523741+SoheebAmin@users.noreply.github.com>
2023-01-01 17:56:45 -0600

    updated beta link set-up

--- src/components/Navigationbar/Navigationbar.tsx
+++ src/components/Navigationbar/Navigationbar.tsx
@@ -1,19 +1,13 @@
 import logo from '../../images/logo-4x.png';
 import './Navigation.css';
-import { Link } from 'react-router-dom';
 
 export default function Navigationbar() {
   return (
     <div className="header_container">
-      <Link to="/">
         <img className="contrax_logo img-fluid"  alt="contrax-logo" src={logo} />
-      </Link>
-      {/* TODO - Re-add link to 'Enter App' once beta is ready */}
-      {/* <Link to="/application"> */}
-      <div className="enter_button text-center"  style={{color: "White"}}>
-          <p><a href="https://beta.contrax.finance">Enter App</a></p>
-      </div>
-      {/* </Link> */}
+      <a href="https://beta.contrax.finance" className="enter_button text-center">
+        <p>Enter App</p>
+      </a>
     </div>
   );
 }
